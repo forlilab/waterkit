9b6d819fbd500fa1785905512849a3423aa0455b
Jérôme Eberhardt <qksoneo@gmail.com>
2017-10-25 18:06:52 -0700

    add optimize function; add anchor_type

--- water.py
+++ water.py
@@ -16,10 +16,13 @@ from molecule import Molecule
 
 class Water(Molecule):
 
-    def __init__(self, oxygen, anchor):
+    def __init__(self, oxygen, anchor, anchor_type):
+        # Create ob molecule and add oxygen atom
         self._OBMol = ob.OBMol()
         self.add_atom(oxygen, atomic=8)
-        self._anchor = anchor
+        # Store all the informations about the anchoring
+        self._anchor = np.array([anchor, anchor + utils.normalize(utils.vector(oxygen, anchor))])
+        self._anchor_type = anchor_type
 
     def add_atom(self, xyz, atomic, bond=None):
         a = self._OBMol.NewAtom()
@@ -29,18 +32,41 @@ class Water(Molecule):
         if bond is not None and self._OBMol.NumAtoms() >= 1:
             self._OBMol.AddBond(bond[0], bond[1], bond[2])
 
-    def get_coordinate(self, atom_id=None):
+    def coordinates(self, atom_id=None):
         if atom_id is not None:
-            ob_atom = self._.OBMol.GetAtomById(atom_id)
+            ob_atom = self._OBMol.GetAtomById(atom_id)
             coordinate = [ob_atom.GetX(), ob_atom.GetY(), ob_atom.GetZ()]
         else:
             coordinate = [[x.GetX(), x.GetY(), x.GetZ()] for x in ob.OBMolAtomIter(self._OBMol)]
 
-        return np.array(coordinate)
+        return np.atleast_2d(np.array(coordinate))
 
-    def update_coordinate(xyz, atom_id):
+    def update_coordinates(xyz, atom_id):
         ob_atom = self._OBMol.GetAtomById(atom_id)
         ob_atom.SetVector(xyz[0], xyz[1], xyz[2])
+
+    def optimize(self, ad_map, radius=3., angle=110., ignore=0):
+        # Get oxygen coordinate
+        coordinate = self.coordinates(atom_id=0)
+        # Get energy of the current position
+        energy = ad_map.get_energy(coordinate)
+
+        # Get all the point around a coordinate (sphere)
+        ad_map.get_neighbor_points(coordinate, radius)
+
+        """
+        # Compute angle
+        angles = utils.get_angle(coordinates, self._anchor[0], self._anchor[1])
+        # Keep only coordinates within the angle
+        idx = np.array((angles <= angle).nonzero()).T
+
+        if new_energy < energy:
+            energy = new_energy
+            coordinate = new_coordinate
+
+        if energy < ignore:
+            self.update_coordinates(coordinate, atom_id=0)
+        """
 
     def build_hydrogens(self):
         pass
@@ -50,11 +76,3 @@ class Water(Molecule):
 
     def scan(self, ref_id=1):
         pass
-
-def main():
-    w = Water(oxygen=[0, 0, 0], anchor=[[0, 0, 1], [0, 0, 2]])
-    w.build_hydrogens()
-    w.to_file("outputfile.pdb", "pdb")
-
-if __name__ == '__main__':
-    main()
