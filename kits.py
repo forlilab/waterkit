e21359d26c342bebc416ba54afba1976bd133615
Jérôme Eberhardt <qksoneo@gmail.com>
2017-11-03 19:16:50 -0700

    start to use SMARTS pattern instead of my previous soup to place water molecules

--- kits.py
+++ kits.py
@@ -16,13 +16,15 @@ import utils
 from autodock_map import Autodock_map
 from molecule import Molecule
 from water import Water
+from waterfield import Waterfield
 
 
 class Kits():
 
-    def __init__(self, vdw_water=2.8):
+    def __init__(self, waterfield, vdw_water=2.8):
         
         self._vdw_water = vdw_water
+        self._waterfield = waterfield
     
     def _place_optimal_water(self, molecule, ad_map=None):
         
@@ -33,9 +35,36 @@ class Kits():
         hb_donor_length = 2.8 # if water is donor (distance Woxygen(-Whydrogen) -- Acceptor)
         hb_acceptor_length = 1.8 # if water is acceptor (distance Woxygen -- hydrogen(-Donor))
 
-        # Get all the residues in the map
-        idx = molecule.get_residues_in_map(ad_map)
+        # Get all the atoms in the map
+        idx_map = molecule.get_atoms_in_map(ad_map)
+        # Get all the water types from the waterfield
+        atom_types = self._waterfield.get_atom_types()
 
+        # In order to keep track which one was alredy typed or not
+        visited = [False] * (molecule._OBMol.NumAtoms() + 1)
+
+        # We will test every patterns. Not very efficient 
+        # but otherwise we have to do the contrary, test if the atom 
+        # is in the pattern list or not... more complicated
+        for i in atom_types.iterkeys():
+
+            n_water = atom_types[i][2]
+            ob_smarts = atom_types[i][3]
+
+            ob_smarts.Match(molecule._OBMol)
+            matches = list(ob_smarts.GetMapList())
+
+            for match in matches:
+                idx = match[0]
+
+                print match
+
+                if idx in idx_map and not visited[idx]:
+                    visited[idx] = True
+
+
+
+        """
         # Place first class water molecules
         for i in idx:
             
@@ -53,7 +82,7 @@ class Kits():
                 id_atom = ob_atom.GetId()
                 coord_atom = np.array([ob_atom.GetX(), ob_atom.GetY(), ob_atom.GetZ()])
                 hyb_atom = ob_atom.GetHyb()
-
+                val_atom = ob_atom.GetValence()
                 
                 if ob_atom.IsHbondDonorH():
                     # Get the coordinates of the neighbor atom
@@ -67,7 +96,7 @@ class Kits():
                     p = utils.atom_to_move(coord_atom, coord_neighbor_atoms[1])
                     angles = [0]
                 
-                if ob_atom.IsHbondAcceptorSimple():
+                if ob_atom.IsHbondAcceptor():
                     # Get the coordinates of the neighbor atom
                     coord_neighbor_atoms = molecule.get_neighbor_atom_coordinates(id_atom, 2)
                     # Set HBond length
@@ -127,7 +156,7 @@ class Kits():
                                 waters.append(w)
 
                     angles = []
-        
+        """
         return waters
     
     def hydrate(self, molecule, ad_map=None):
@@ -164,6 +193,8 @@ def cmd_lineparser():
                         action="store", help="molecule file")
     parser.add_argument("-m", "--map", dest="map_file", default=None,
                         action="store", help="autodock map file")
+    parser.add_argument("-w", "--water", dest="waterfield_file", required=True,
+                        action="store", help="waterfield file")
 
     return parser.parse_args()
 
@@ -173,14 +204,17 @@ def main():
 
     pdbqt_file = args.pdbqt_file
     map_file = args.map_file
+    waterfield_file = args.waterfield_file
 
     # Read PDBQT file
     molecule = Molecule(pdbqt_file)
     # Read AutoDock grid map
     ad_map = Autodock_map(map_file)
+    # Read the Waterfield file
+    waterfield = Waterfield(waterfield_file)
 
     # Go kits!!
-    k = Kits()
+    k = Kits(waterfield)
     waters = k.hydrate(molecule, ad_map)
 
     utils.write_water('waters.pdb', waters)
