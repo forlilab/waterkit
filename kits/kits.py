42dce0450b04c3faee09ddcdf40269db3b8c1978
Jérôme Eberhardt <qksoneo@gmail.com>
2018-02-20 20:01:46 -0800

    store waterfield with namedtuple

--- kits/kits.py
+++ kits/kits.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Kits
+# WaterKits
 #
-# The core of the kits program
+# The core of the WaterKits program
 #
 
 
@@ -23,16 +23,18 @@ from optimize import Water_network
 class Kits():
 
     def __init__(self, waterfield, water_map):
-        
         self._water_map = water_map
         self._waterfield = waterfield
     
-    def _place_optimal_water(self, molecule, idx, hyb, n_water, hb_type, hb_length):
-
+    def _place_optimal_water(self, molecule, atom_type, idx):
+        """Place one or multiple water molecules in the ideal position 
+        above an acceptor or donor atom
+        """
         waters = []
         angles = []
+        hyb = atom_type.hyb
         
-        if hb_type == 1:
+        if atom_type.hb_type == 1:
             anchor_type = 'donor'
         else:
             anchor_type = 'acceptor'
@@ -45,29 +47,27 @@ class Kits():
         coord_neighbor_atoms = molecule.get_neighbor_atom_coordinates(idx, depth=2)
 
         if hyb == 1:
-
             coord_atom1 = coord_neighbor_atoms[1][0]
 
             # Position of water is linear
             # And we just need the origin atom and the first neighboring atom
             # Example: H donor
-            if n_water == 1:
+            if atom_type.n_water == 1:
                 r = None
                 p = coord_atom + utils.vector(coord_atom1, coord_atom)
                 angles = [0]
 
-            if n_water == 3:
+            if atom_type.n_water == 3:
                 hyb = 3
 
         if hyb == 2:
-
             # Position of water is just above the origin atom
             # We need the 2 direct neighboring atoms of the origin atom
             # Example: Nitrogen
-            if n_water == 1:
+            if atom_type.n_water == 1:
                 coord_atom1 = coord_neighbor_atoms[1][0]
                 coord_atom2 = coord_neighbor_atoms[1][1]
-
+                #
                 r = None
                 p = utils.atom_to_move(coord_atom, [coord_atom1, coord_atom2])
                 angles = [0]
@@ -83,11 +83,10 @@ class Kits():
                 p = coord_atom1
                 angles = [-np.radians(120), np.radians(120)]
 
-            elif n_water == 3:
+            elif atom_type.n_water == 3:
                 hyb = 3
 
         if hyb == 3:
-
             #print idx, hb_type
             coord_atom1 = coord_neighbor_atoms[1][0]
             coord_atom2 = coord_neighbor_atoms[1][1]
@@ -95,7 +94,7 @@ class Kits():
             # Position of water is just above the origin atom
             # We need the 3 direct neighboring atoms (tetrahedral)
             # Exemple: Ammonia
-            if n_water == 1:
+            if atom_type.n_water == 1:
                 coord_atom3 = coord_neighbor_atoms[1][2]
 
                 # We have to normalize bonds, otherwise the water molecule is not well placed
@@ -110,8 +109,7 @@ class Kits():
             # Position of waters are separated by angle of 109 degrees
             # Tetrahedral geometry, perpendicular with the neighboring atoms of the origin atom
             # Example: Oxygen of the hydroxyl group
-            elif n_water == 2:
-
+            elif atom_type.n_water == 2:
                 v1 = coord_atom + utils.normalize(utils.vector(coord_atom, coord_atom1))
                 v2 = coord_atom + utils.normalize(utils.vector(coord_atom, coord_atom2))
 
@@ -122,8 +120,7 @@ class Kits():
             # Position of waters are separated by angle of 109 degrees
             # Tetrahedral geometry, there is no reference so water molecules are placed randomly
             # Example: DMSO
-            elif n_water == 3:
-
+            elif atom_type.n_water == 3:
                 coord_atom1 = coord_neighbor_atoms[1][0]
 
                 # Vector between coord_atom and the only neighbor atom
@@ -135,7 +132,7 @@ class Kits():
                 r = coord_atom + utils.get_perpendicular_vector(v)
 
                 # And we place a pseudo atom (will be the first water molecule)
-                p = utils.rotate_atom(coord_atom1, coord_atom, r, np.radians(109.47), hb_length)
+                p = utils.rotate_atom(coord_atom1, coord_atom, r, np.radians(109.47), atom_type.hb_length)
                 # The next rotation axis will be the vector along the neighbor atom and the origin atom 
                 r = coord_atom + utils.normalize(utils.vector(coord_atom1, coord_atom))
 
@@ -143,107 +140,94 @@ class Kits():
 
         # Now we place the water molecule(s)!
         for angle in angles:
-            w = utils.rotate_atom(p, coord_atom, r, angle, hb_length)
+            w = utils.rotate_atom(p, coord_atom, r, angle, atom_type.hb_length)
             # Create water molecule
             waters.append(Water(w, anchor=coord_atom, anchor_type=anchor_type))
 
         return waters
     
     def hydrate(self, molecule, ad_map):
-
-        first_waters = []
+        """Hydrate the molecule by adding successive layers 
+        of water molecules until the box is complety full
+        """
+        waters = []
 
         # First hydration shell!!
-        # Place optimal water molecules everywhere
 
         # Get all the atoms in the map
         idx_map = molecule.get_atoms_in_map(ad_map)
         # Get all the water types from the waterfield
         atom_types = self._waterfield.get_atom_types()
-
         # In order to keep track which one was alredy typed or not
         visited = [False] * (molecule._OBMol.NumAtoms() + 1)
 
         # We will test every patterns. Not very efficient 
         # but otherwise we have to do the contrary, test if the atom 
         # is in the pattern list or not... more complicated
-        for i in atom_types.keys()[::-1]:
-
-            hb_type = atom_types[i][1]
-            weight = atom_types[i][2]
-            hyb = atom_types[i][3]
-            n_water = atom_types[i][4]
-            hb_length = atom_types[i][5]
-            ob_smarts = atom_types[i][6]
-
-            ob_smarts.Match(molecule._OBMol)
-            matches = list(ob_smarts.GetMapList())
+        for key in atom_types.keys()[::-1]:
+            atom_type = atom_types[key]
+            matches = self._waterfield.get_matches(key, molecule)
 
             for match in matches:
                 idx = match[0]
 
-                if hb_type == 0 and not visited[idx]:
+                if atom_type.hb_type == 0 and not visited[idx]:
                     visited[idx] = True
-                    #print '%10s %2d %s' % (atom_types[i][0], idx, 'NO')
 
                 if idx in idx_map and not visited[idx]:
-                    #print '%10s %2d %s' % (atom_types[i][0], idx, 'YES WATER!')
                     visited[idx] = True
+
                     try:
-                        first_waters.extend(self._place_optimal_water(molecule, idx, hyb, n_water, hb_type, hb_length))
+                        waters.extend(self._place_optimal_water(molecule, atom_type, idx))
                     except:
+                        print 'Error: Couldn\'t put water(s) on %s using %s atom type' % (idx, key)
                         continue
 
         # We optimize the water placement
         # and keep only the favorable ones (energy < 0)
-        n = Water_network(distance=2.8, angle=90, cutoff=0)
-        first_waters = n.optimize(first_waters, ad_map)
+        n = Water_network(distance=2.8, angle=145, cutoff=0)
+        waters = n.optimize(waters, ad_map)
 
         # Complete map
-        Water.complete_map(first_waters, ad_map, self._water_map)
-
-        ad_map.to_map('HD.map', 'HD')
-        ad_map.to_map('Lp.map', 'Lp')
-        ad_map.to_map('O.map', 'O')
+        Water.complete_map(waters, ad_map, self._water_map)
 
         # Second to N hydration shell!!
-        """
-        n_waters = []
-        current_waters = first_waters
 
-        for n in range(5):
+        add_waters = True
+        current_waters = waters
 
-            print n
-
+        while add_waters:
             tmp_waters = []
 
             # Second hydration shell!!
             for water in current_waters:
                 if water._anchor_type == 'acceptor':
                     atom_ids = [3, 4, 5]
-                    hb_types = [1, 2, 2]
-                    hb_lengths = [1.8, 2.1, 2.1]
+                    names = ['H_O_004', 'O_L_000', 'O_L_000']
                 else:
                     atom_ids = [2, 3, 5]
-                    hb_types = [1, 1, 2]
-                    hb_lengths = [1.8, 1.8, 2.1]
+                    names = ['H_O_004', 'H_O_004', 'O_L_000']
 
-                for idx, hb_type, hb_length in zip(atom_ids, hb_types, hb_lengths):
-                    tmp_waters.extend(self._place_optimal_water(water, idx, 1, 1, hb_type, hb_length))
+                for idx, name in zip(atom_ids, names):
+                    tmp_waters.extend(self._place_optimal_water(water, atom_types[name], idx))
  
-            # If a AD_map is provide, we optimize the water placement
+            # We optimize the water placement
             # and keep only the favorable ones (energy < 0)
-            if ad_map is not None:
-                tmp_waters = self.optimize_waters(tmp_waters, ad_map, radius=[2.4, 3.3], angle=170, cutoff=0)
- 
-            current_waters = tmp_waters
-            n_waters.extend(tmp_waters)
-        """
+            tmp_waters = n.optimize(tmp_waters, ad_map)
+
+            if tmp_waters:
+                # Complete map
+                Water.complete_map(tmp_waters, ad_map, self._water_map)
+
+                current_waters = tmp_waters
+                waters.extend(tmp_waters)
+            else:
+                add_waters = False
  
          # ???
          # PROFIT!
          
-        return first_waters#, n_waters
+        return waters
       
 
 def cmd_lineparser():
@@ -259,24 +243,20 @@ def cmd_lineparser():
                         action="store", help="autodock map file")
     parser.add_argument("-o", "--output", dest="output_file", default='waters.pdbqt',
                         action="store", help="water molecule file (pdbqt)")
-
     return parser.parse_args()
 
 def main():
 
     args = cmd_lineparser()
-
     pdbqt_file = args.pdbqt_file
     map_file = args.map_file
     waterfield_file = args.waterfield_file
     output_file = args.output_file
     water_map_file = args.water_map_file
 
-    # Read PDBQT file
+    # Read PDBQT file, Waterfield file and AutoDock grid map
     molecule = Molecule(pdbqt_file)
-    # Read the Waterfield file
     waterfield = Waterfield(waterfield_file)
-    # Read AutoDock grid map
     ad_map = Autodock_map(map_file)
     water_map = Autodock_map(water_map_file)
 
@@ -284,7 +264,11 @@ def main():
     k = Kits(waterfield, water_map)
     waters = k.hydrate(molecule, ad_map)
 
+    # Write output files
     utils.write_water(output_file, waters)
+    ad_map.to_map('HD.map', 'HD')
+    ad_map.to_map('Lp.map', 'Lp')
+    ad_map.to_map('O.map', 'O')
 
 if __name__ == '__main__':
     main()
--- kits/waterfield.py
+++ kits/waterfield.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Kits
+# WaterKits
 #
 # Class to manage water forcefield
 #
 
 import re
+
+from collections import namedtuple
+from collections import OrderedDict
 
 import numpy as np
 import openbabel as ob
@@ -15,16 +18,17 @@ import openbabel as ob
 class Waterfield():
 
     def __init__(self, fname):
+        # Create ordered dict to conserved the order of atom types
+        # which is very important in our particular case for the moment
+        self._atom_types = OrderedDict()
+        field_names = 'hb_type hb_strength hyb n_water hb_length ob_smarts'
+        self._Atom_type = namedtuple('Atom_type', field_names)
 
-        self._atom_types = {}
-        self.load_files(fname)
+        self._load_param_file(fname)
 
-    def load_files(self, fname):
+    def _load_param_file(self, fname):
+        """Load the file to create a water Forcefield object
         """
-        Load one or more files to create a Forceflied object based on them
-        """
-        i = 0
-
         with open(fname) as f:
             lines = f.readlines()
 
@@ -39,28 +43,67 @@ class Waterfield():
 
                     name = sline[1]
                     hb_type = np.int(sline[2])
-                    strength = np.float(sline[3])
+                    hb_strength = np.float(sline[3])
                     hyb = np.int(sline[4])
                     n_water = np.int(sline[5])
                     hb_length = np.float(sline[6])
-                    smarts =  sline[7]
+                    smarts = sline[7]
 
-                    # Initialize the SMARTS pattern and check if is valid
-                    ob_smarts = ob.OBSmartsPattern()
-                    success = ob_smarts.Init(smarts)
+                    if smarts == '[]':
+                        smarts = None
 
-                    # Add atom type if is valid
-                    if success:
-                        self._atom_types[i] = [name, hb_type, strength, hyb, n_water, hb_length, ob_smarts]
-                        i += 1
-                    else:
-                        print "Warning: SMARTS %s is invalid" % smarts
+                    # And add it
+                    self.add_atom_type(name, hb_type, hb_strength, hyb, n_water, hb_length, smarts)
 
-    def get_atom_types(self, idx=None):
+    def create_atom_type(self, hb_type, hb_strength, hyb, n_water, hb_length, smarts=None):
+        """Create atom type using a namedtuple
         """
-        Return all the atom types or just one based on the idx
+        if smarts is not None:
+            # Initialize the SMARTS pattern and check if is valid
+            ob_smarts = ob.OBSmartsPattern()
+            success = ob_smarts.Init(smarts)
+
+            # Add atom type if is valid
+            if success:
+                atom_type = self._Atom_type(hb_type, hb_strength, hyb, n_water, hb_length, ob_smarts)
+            else:
+                print "Warning: SMARTS %s is invalid" % smarts
+                return None
+        else:
+            atom_type = self._Atom_type(hb_type, hb_strength, hyb, n_water, hb_length, None)
+
+        return atom_type
+
+    def add_atom_type(self, name, hb_type, hb_strength, hyb, n_water, hb_length, smarts=None):
+        """Add an atom type to the atom types library
         """
-        if idx is None:
+        atom_type = self.create_atom_type(hb_type, hb_strength, hyb, n_water, hb_length, smarts)
+
+        if atom_type is not None:
+            self._atom_types[name] = atom_type
+
+    def get_atom_types(self, name=None):
+        """Return all the atom types or just one based on the idx
+        """
+        if name is None:
             return self._atom_types
         else:
-            return self._atom_types[idx]
+            return self._atom_types[name]
+
+    def get_matches(self, name, molecule):
+        """Return matches using smarts on the molecule
+        """
+        if self._atom_types.has_key(name):
+            atom_type = self._atom_types[name]
+
+            if atom_type.ob_smarts is not None:
+                atom_type.ob_smarts.Match(molecule._OBMol)
+                matches = list(atom_type.ob_smarts.GetMapList())
+            else:
+                matches = []
+        else:
+            matches = []
+
+        return matches
+
+
