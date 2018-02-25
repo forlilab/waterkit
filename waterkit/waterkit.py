54612a89cf544251e9de4feb307d56d81e3a1f6e
Jérôme Eberhardt <qksoneo@gmail.com>
2018-02-24 21:19:26 -0800

    it is not necessary to provide waterfield and water map anymore

--- waterkit/waterkit.py
+++ waterkit/waterkit.py
@@ -6,7 +6,8 @@
 # The core of the WaterKit program
 #
 
-
+import os
+import sys
 import argparse
 
 import numpy as np
@@ -185,7 +186,7 @@ class Waterkit():
 
             # Second hydration shell!!
             for water in previous_waters:
-                names, atom_ids = water.get_available_anchors(self._waterfield)
+                names, atom_ids = water.get_available_anchors()
 
                 for name, idx in zip(names, atom_ids):
                     atom_type = self._waterfield.get_atom_types(name)
@@ -212,32 +213,40 @@ class Waterkit():
       
 
 def cmd_lineparser():
-    parser = argparse.ArgumentParser(description='kits')
-
-    parser.add_argument("-p", "--pdbqt", dest="pdbqt_file", required=True,
+    parser = argparse.ArgumentParser(description='waterkit')
+    parser.add_argument("-i", "--mol", dest="mol_file", required=True,
                         action="store", help="molecule file")
-    parser.add_argument("-f", "--waterfield", dest="waterfield_file", required=True,
-                         action="store", help="waterfield file")
-    parser.add_argument("-w", "--watermap", dest="water_map_file", required=True,
-                        action="store", help="water autodock map file")
     parser.add_argument("-m", "--map", dest="map_file", required=True,
                         action="store", help="autodock map file")
     parser.add_argument("-o", "--output", dest="output_file", default='waters.pdbqt',
                         action="store", help="water molecule file (pdbqt)")
+    parser.add_argument("-f", "--waterfield", dest="waterfield_file", default=None,
+                         action="store", help="waterfield file")
+    parser.add_argument("-w", "--watermap", dest="water_map_file", default=None,
+                        action="store", help="water autodock map file")
     return parser.parse_args()
 
 def main():
     args = cmd_lineparser()
-    pdbqt_file = args.pdbqt_file
+    mol_file = args.mol_file
     map_file = args.map_file
     waterfield_file = args.waterfield_file
     output_file = args.output_file
     water_map_file = args.water_map_file
 
-    # Read PDBQT file, Waterfield file and AutoDock grid map
-    molecule = Molecule(pdbqt_file)
-    waterfield = Waterfield(waterfield_file)
+    # Read PDBQT/MOL2 file, Waterfield file and AutoDock grid map
+    molecule = Molecule(mol_file)
     ad_map = Autodock_map(map_file)
+
+    d = os.path.dirname(sys.modules[__name__].__file__)
+
+    if waterfield_file is None:
+        waterfield_file = os.path.join(d, 'data/waterfield.par')
+
+    if water_map_file is None:
+        water_map_file = os.path.join(d, 'data/water/maps.fld')
+
+    waterfield = Waterfield(waterfield_file)
     water_map = Autodock_map(water_map_file)
 
     # Go waterkit!!
