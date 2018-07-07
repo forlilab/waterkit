ea31087220819a5cfd349c30b48dee272e251a96
Jérôme Eberhardt <qksoneo@gmail.com>
2018-07-07 16:58:31 -0700

    remove output csv file with residues list, wrong good idea; possibility to give a csv file with the list of residues (resid,segid) that compose the pocket of interest

--- scripts/get_grid_parameters.py
+++ scripts/get_grid_parameters.py
@@ -14,13 +14,32 @@ def cmd_lineparser():
     parser = argparse.ArgumentParser(description='grid parameters')
     parser.add_argument("-i", "--mol", dest="mol_file", required=True,
                         action="store", help="molecule file")
-    parser.add_argument("-s", "--selection", dest="selection", required=True,
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-s", "--selection", dest="selection",
                         action="store", help="MDAnalysis selection string")
+    group.add_argument("-l", "--residues", dest="csv_file",
+                        action="store", help="csv file with residues (resid,segid)")
     parser.add_argument("--buffer", dest="buffer_space", default=11, type=int,
                         action="store", help="buffer spacing")
     parser.add_argument("--spacing", dest="spacing", default=0.375, type=float,
                         action="store", help="grid spacing")
     return parser.parse_args()
+
+
+def read_residues_csv_file(csv_file):
+    residues = []
+
+    with open(csv_file) as f:
+        lines = f.readlines()
+
+        for line in lines:
+            try:
+                resid, segid = line.split(',')
+                residues.append((int(resid), segid.rstrip()))
+            except:
+                continue
+
+    return residues
 
 
 def get_npts(box, center, spacing, buffer_space=0):
@@ -45,7 +64,7 @@ def get_npts(box, center, spacing, buffer_space=0):
     x = np.floor(x / spacing) * 2
     y = np.floor(y / spacing) * 2
     z = np.floor(z / spacing) * 2
-    
+
     npts = np.array([np.int(x), np.int(y), np.int(z)])
 
     return npts
@@ -55,11 +74,18 @@ def main():
     args = cmd_lineparser()
     mol_file = args.mol_file
     selection = args.selection
+    csv_file = args.csv_file
     buffer_space = args.buffer_space
     spacing = args.spacing
 
     u = Universe(mol_file)
-    ligand = u.select_atoms(selection)
+
+    if selection:
+        ligand = u.select_atoms(selection)
+    elif csv_file:
+        residues = read_residues_csv_file(csv_file)
+        selection = ' or '.join(['(resid %d and segid %s)' % residue for residue in residues])
+        ligand = u.select_atoms(selection)
         
     # Get the center/box of the selection
     center = ligand.centroid(pbc=False)
@@ -69,16 +95,6 @@ def main():
 
     print 'npts %d %s %d' % (npts[0], npts[1], npts[2])
     print 'gridcenter %5.3f %5.3f %5.3f' % (center[0], center[1], center[2])
-
-    # Get residues in the pocket
-    pocket_selection = '(protein or nucleic) and around 4 %s' % selection
-    pocket = u.select_atoms(pocket_selection)
-    residues = set([(x.resid, x.segid) for x in pocket])
-
-    with open('pocket_residues.csv', 'w') as w:
-        w.write('resid,segid\n')
-        for residue in residues:
-            w.write('%s,%s\n' % residue)
 
 if __name__ == '__main__':
     main()
