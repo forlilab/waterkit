6cd0218e2390f12dc50155a5ab3b1e0eec34a457
Jérôme Eberhardt <qksoneo@gmail.com>
2018-08-24 17:08:23 -0700

    add sort=False to append pandas function

--- waterkit/water_box.py
+++ waterkit/water_box.py
@@ -166,7 +166,7 @@ class WaterBox():
     def add_informations(self, data, where):
         """ Append DF to the existing information DF """
         try:
-            self.df[where] = self.df[where].append(data)
+            self.df[where] = self.df[where].append(data, sort=False)
             self.df[where].reset_index(drop=True, inplace=True)
         except:
             print "Error: Cannot add informations to %s dataframe." % where
