dbe6d7a6389d0e26388d7f7ce65a13d7702e8c22
jeeberhardt <qksoneo@gmail.com>
2019-08-26 16:23:53 -0700

    update version to 0.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ author = u'Jerome Eberhardt'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.1'
+release = u'0.3'
 
 
 # -- General configuration ---------------------------------------------------
