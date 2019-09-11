e493f975cd34ad509114a6406b4228f6776e05cf
jeeberhardt <qksoneo@gmail.com>
2019-09-10 18:59:10 -0700

    make_trajectory, amber2pdbqt and run_waterkit are command line tools

--- setup.py
+++ setup.py
@@ -5,34 +5,37 @@ import os
 from setuptools import setup, find_packages
 
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
-REQUIREMENTS_FILE = os.path.join(PROJECT_ROOT, 'requirement.txt')
+REQUIREMENTS_FILE = os.path.join(PROJECT_ROOT, "requirement.txt")
 
 with open(REQUIREMENTS_FILE) as f:
    install_reqs = f.read().splitlines()
 
-install_reqs.append('setuptools')
+install_reqs.append("setuptools")
 
-setup(name='waterkit',
+setup(name="waterkit",
       version=0.3,
-      description='WaterKit',
-      author='Jerome Eberhardt',
-      author_email='jerome@scripps.edu',
-      url='https://github.com/jeeberhardt/waterkit',
+      description="WaterKit",
+      author="Jerome Eberhardt",
+      author_email="jerome@scripps.edu",
+      url="https://github.com/jeeberhardt/waterkit",
       packages=find_packages(),
-      data_files = [('', ['waterkit/data/AD4_parameters.dat',
-                          'waterkit/data/disordered_hydrogens.par',
-                          'waterkit/data/waterfield.par',
-                          'waterkit/data/water_orientations.txt'])],
+      scripts=["scripts/amber2pdbqt.py",
+               "scripts/make_trajectory.py",
+               "scripts/run_waterkit.py"],
+      data_files = [("", ["waterkit/data/AD4_parameters.dat",
+                          "waterkit/data/disordered_hydrogens.par",
+                          "waterkit/data/waterfield.par",
+                          "waterkit/data/water_orientations.txt"])],
       install_requires=install_reqs,
       include_package_data=True,
       zip_safe=False,
-      license='MIT',
-      keywords=['molecular modeling', 'drug design',
-               'docking', 'autodock'],
+      license="MIT",
+      keywords=["molecular modeling", "drug design",
+               "docking", "autodock"],
       classifiers=[
-            'Programming Language :: Python :: 2.7',
-            'Operating System :: Unix',
-            'Operating System :: MacOS',
-            'Topic :: Scientific/Engineering'
+            "Programming Language :: Python :: 2.7",
+            "Operating System :: Unix",
+            "Operating System :: MacOS",
+            "Topic :: Scientific/Engineering"
       ]
 )
