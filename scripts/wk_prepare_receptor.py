e5d4d44ff29f80bd594e9f48c87e8378fcd8338d
jeeberhardt <qksoneo@gmail.com>
2020-08-06 21:46:23 -0700

    change 2C and 3C atom types to C2 and C3 for OB

--- scripts/wk_prepare_receptor.py
+++ scripts/wk_prepare_receptor.py
@@ -127,8 +127,15 @@ def write_pdbqt_file(output_name, molecule):
 
         resname = atom.residue.name
         resid = atom.residue.idx + 1
+
+        # OpenBabel does not like atom types starting with a number
+        if atom.type[0].isdigit():
+            atom_type = atom.type[::-1]
+        else:
+            atom_type = atom.type
+
         # AutoGrid does not accept atom type name of length > 2
-        atom_type = atom.type[:2]
+        atom_type = atom_type[:2]
 
         if resname in RESSOLV:
             atype = "HETATM"
