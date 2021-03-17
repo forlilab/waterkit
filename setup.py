cfc15fafa4882987a27d7b2f283807db443981c9
jeeberhardt <qksoneo@gmail.com>
2021-03-16 18:09:34 -0700

    Changed version to 0.5

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def find_files(directory):
 
 
 setup(name="waterkit",
-      version=0.4,
+      version=0.5,
       description="WaterKit",
       author="Jerome Eberhardt",
       author_email="jerome@scripps.edu",
